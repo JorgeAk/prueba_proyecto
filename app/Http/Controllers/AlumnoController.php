6a8b72bd387075e6d96090a3e205d682632f0a79
JorgeAk <soul_japon@hotmail.com>
2021-06-22 00:59:09 -0500

    chg

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -173,13 +173,13 @@ class AlumnoController extends Controller
     public function redirec()
     {
         $mensajes_rec = $this->verificar_mensajes();
-        if (Auth::check()) {
+        //if (Auth::check()) {
             //return view('alumnos.index');
-            return redirect()->guest('/alumnos');
-        }else{
-            return view('alumnos.login', compact('mensajes_rec'));
+            //return redirect()->guest('/alumnos');
+        //}else{
+            return view('alumnos.login');
 
-        }
+        //}
         
     }
 
